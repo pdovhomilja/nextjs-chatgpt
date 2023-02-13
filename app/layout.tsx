02a6f9810f18102bf5317eb120e3b4ea38a678ac
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2023-02-13 23:34:17 +0100

    Connected to ChatGPT API

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../pages/api/auth/[...nextauth]";
 import "../styles/globals.css";
 import Login from "../components/Login";
+import ClientProvider from "../components/ClientProvider";
 
 export default async function RootLayout({
   children,
@@ -25,6 +26,7 @@ export default async function RootLayout({
               <div className="bg-[#202123] max-w-xs hs-screen overflow-y-auto md:min-w-[20rem]">
                 <SideBar />
               </div>
+              <ClientProvider />
               {/* Chat */}
               <div className="bg-[#343541] flex-1">{children}</div>
             </div>
