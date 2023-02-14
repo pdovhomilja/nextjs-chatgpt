88e501bea80acc0a7a4c9a78a21e5cb6bac4f731
pdovhomilja <41537049+pdovhomilja@users.noreply.github.com>
2023-02-14 11:21:07 +0100

    final build

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -41,14 +41,31 @@ const SideBar = () => {
           </div>
         </div>
       </div>
-      {session && (
-        <img
-          onClick={() => signOut()}
-          src={session.user?.image!}
-          alt="user"
-          className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50"
-        />
-      )}
+      <div>
+        <div className="flex flex-row items-center space-x-5 mx-5 justify-start">
+          {session && (
+            <img
+              onClick={() => signOut()}
+              src={session.user?.image!}
+              alt="user"
+              className="h-8 w-8 rounded-full cursor-pointer mb-2 hover:opacity-50"
+            />
+          )}
+          {session && (
+            <div>
+              <p className="text-white text-center text-sm">
+                {session.user?.name}
+              </p>
+              <p className="text-white text-center text-xs">
+                {session.user?.email}
+              </p>
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="flex flex-col items-center justify-center pt-4">
+        <p className="text-white text-center text-xs">version: 0.0.1</p>
+      </div>
     </div>
   );
 };
